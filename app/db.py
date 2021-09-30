b2ec1e3984ea9bc17e8afd7b84a5eb46529d3a49
Dung Nguyen <ntdung2508@gmail.com>
2021-09-30 16:24:28 +0700

    fix

--- app/db.py
+++ app/db.py
@@ -28,8 +28,9 @@ def get_db():
             # password="abcd1234",
             maxPoolSize=50,  # Set the maximum connection pool size to 50 active connections.
             w='majority',  # Set the write timeout limit to 2500 milliseconds.
-            wtimeout=2500,
-            ssl_ca_certs=certifi.where()
+            wtimeout=2500
+            # ,
+            # ssl_ca_certs=certifi.where()
         )[APP_DB_NAME]
     return db
 
